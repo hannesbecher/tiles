29470f8f7533e89554d625bae9240c61c19d418b
hannesbecher <hannesbecher@gmx.de>
2024-09-28 20:07:13 +0100

    tilesAndSomeCode

---
+++ code/01imge.R
@@ -0,0 +1,14 @@
+
+# git to make image magick make a tiled "montage"
+setwd("~/git_repos/tiles/tiles/")
+
+
+ff <- dir(pattern="*.svg.png")
+n <- 100
+#wgt <- rev(rep(c(5:1)^3, each=2))
+wgt <- rep(1, 10)
+samp <- sample(ff, size = n, replace = T, prob = wgt)
+ffString <- paste(samp, collapse=" ")
+oString <- "out.png"
+mString <- paste0("magick montage ", ffString, " -geometry +0+0 -border 0 ", oString)
+write(mString, "montage.sh")
